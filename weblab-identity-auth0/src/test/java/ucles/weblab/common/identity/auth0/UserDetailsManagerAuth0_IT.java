f4a628696e8af0cd4c7ce05d934b10b36a265198
Neale Upstone <neale@nealeupstone.com>
2017-12-07 14:38:21 +0000

    RES-1113 Move Auth0Settings to src/main not src/test

--- weblab-identity-auth0/src/test/java/ucles/weblab/common/identity/auth0/UserDetailsManagerAuth0_IT.java
+++ weblab-identity-auth0/src/test/java/ucles/weblab/common/identity/auth0/UserDetailsManagerAuth0_IT.java
@@ -4,7 +4,6 @@ import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
